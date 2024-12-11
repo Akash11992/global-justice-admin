cf853554ad0125dc5330f5a4cf596c31a27580c2
nadeem <nadeem@titan-consultancy.com>
2024-12-11 11:38:28 +0530

    bugs fixed on left penal commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://apis.pharmapreconnectcongress.com/api/v1',   //production
+  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
   apiUrl: 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  // apiUrl: 'http://localhost:5000/api/v1',  //local
   apiMockUrl: '/api/v1',
   domainUrl: 'http://localhost:5000/',
   app_env: 'development',
