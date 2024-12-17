9b5d0ed2d232d4a030cf800f66a406b32f93c37c
Kumar Saurabh <saurabhkumar350@gmail.com>
2024-12-17 16:20:14 +0530

    add discount coupon master component

---
+++ src/app/admin/components/add-discount-coupan/add-discount-coupan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-discount-coupan',
+  templateUrl: './add-discount-coupan.component.html',
+  styleUrls: ['./add-discount-coupan.component.css']
+})
+export class AddDiscountCoupanComponent {
+
+}
