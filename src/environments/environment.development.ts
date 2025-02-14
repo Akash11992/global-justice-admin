dd0ba9ece4b950aa96bff6eb29518bfd2269faea
Brijesh <brijesh@example.com>
2025-02-14 18:45:28 +0530

    resolve done

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'http://localhost:4000/api/v1',   //development
+  // apiUrl: 'http://localhost:4000/api/v1',   //development
+  apiUrl:'https://devglobaljusticeapis.cylsys.com/api/v1',
   apiMockUrl:'/api/v1',
   domainUrl:'http://localhost:5000/',
   app_env: 'development'
