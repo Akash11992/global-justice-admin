a12b3ebeae2d4526125da6dc9ab3ae81231e7ab1
nadeem <nadeem@titan-consultancy.com>
2025-02-27 15:02:15 +0530

    Login UI for live

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,6 @@ export const environment = {
   production: false,
 
   // apiUrl: 'http://localhost:4000/api/v1',   //development
-  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
-
   apiUrl:'https://devglobaljusticeapis.cylsys.com/api/v1',//development
   apiMockUrl:'/api/v1',
   domainUrl:'https://globaljusticeuat.cylsys.com',
