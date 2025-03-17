a173c057e8a7145943789d6257fa185cc5652ec8
nadeem <nadeem@titan-consultancy.com>
2025-03-17 22:45:52 +0530

    integrate QR code and image pdf download on peacekeeper page

--- src/app/features/admin/components/peacekeeper-user/peacekeeper-user.component.ts
+++ src/app/features/admin/components/peacekeeper-user/peacekeeper-user.component.ts
@@ -546,7 +546,23 @@ export class PeacekeeperUserComponent implements OnInit {
     this.userPermissions = this.permissionsService.getStoredPermissions();
   }
 
+  downloadFile(filePath: string, fileName: string, fileType: string) {
+    switch (fileType) {
+      case 'QR':
+        filePath = filePath;
+        break;
 
+      case 'BADGE_IMG':
+        filePath = filePath;
+        break;
+
+      case 'BADGE_PDF':
+        filePath = filePath;
+        break;
+
+    }
+    this.SharedService.downloadFile(filePath, fileName);
+  }
 
 
   openLink(link: any) {
